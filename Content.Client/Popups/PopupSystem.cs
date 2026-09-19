30b8171744e1a8712e1e98a204267c7a90129032
eoineoineoin <helloworld@eoinrul.es>
2026-09-19 21:20:12 +0000

    Fix empty popup texts being raised (#45728)
    * Don't raise empty popups
    
    * Discard whitespace popups, for good measure

--- Content.Client/Popups/PopupSystem.cs
+++ Content.Client/Popups/PopupSystem.cs
@@ -104,7 +104,7 @@ public sealed partial class PopupSystem : SharedPopupSystem
     /// </summary>
     private void PopupInternal(string? message, PopupType type, EntityCoordinates coordinates, EntityUid? entity, bool recordReplay)
     {
-        if (message == null)
+        if (string.IsNullOrWhiteSpace(message))
             return;
 
         if (recordReplay && _replayRecording.IsRecording)
@@ -152,7 +152,7 @@ public sealed partial class PopupSystem : SharedPopupSystem
     /// </summary>
     private void PopupCursorInternal(string? message, PopupType type, bool recordReplay)
     {
-        if (message == null)
+        if (string.IsNullOrWhiteSpace(message))
             return;
 
         if (recordReplay && _replayRecording.IsRecording)
@@ -187,7 +187,7 @@ public sealed partial class PopupSystem : SharedPopupSystem
     /// <param name="type">Used to customize how this popup should appear visually.</param>
     public void PopupCursor(string? message, PopupType type = PopupType.Small)
     {
-        if (!Timing.IsFirstTimePredicted || message is null)
+        if (!Timing.IsFirstTimePredicted || string.IsNullOrWhiteSpace(message))
             return;
 
         _predictionInstances.Add(new PopupCursorEvent.PredictionInstance(message, type, Timing.CurTick));
@@ -214,7 +214,7 @@ public sealed partial class PopupSystem : SharedPopupSystem
 
     public override void PopupCoordinates(string? message, EntityCoordinates coordinates, PopupType type = PopupType.Small, int predictionKey = 0)
     {
-        if (!Timing.IsFirstTimePredicted || message is null)
+        if (!Timing.IsFirstTimePredicted || string.IsNullOrWhiteSpace(message))
             return;
 
         _predictionInstances.Add(new PopupCoordinatesEvent.PredictionInstance(message, type, Timing.CurTick, predictionKey));
@@ -241,7 +241,7 @@ public sealed partial class PopupSystem : SharedPopupSystem
 
     public override void PopupEntity(string? message, EntityUid uid, PopupType type = PopupType.Small)
     {
-        if (!Timing.IsFirstTimePredicted || message is null)
+        if (!Timing.IsFirstTimePredicted || string.IsNullOrWhiteSpace(message))
             return;
 
         if (!TryComp(uid, out TransformComponent? transform))
