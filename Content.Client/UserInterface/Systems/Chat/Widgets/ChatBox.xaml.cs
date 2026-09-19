87d87db4cb42f411e3657adf6b639d46b9db023a
cashew <70486856+cashew-42@users.noreply.github.com>
2026-09-19 21:10:03 +0000

    Fix OnAttachedChanged Lag when having a long chat history (#46050)
    * I fucking hate chat code and UI code
    
    * while I'm at it I'll make this prettier
    
    * docs
    
    * typo
    
    * testfail
    
    * reset chat upon joining game
    
    * move code
    
    * remove unused imports and add doc
    
    * doc
    
    * final commit
    
    * oop
    
    * Revert "reset chat upon joining game"
    
    This reverts commit a851319521193d099b6d1571631b63917500bc47.
    
    * review stuff
    
    * testfail

--- Content.Client/UserInterface/Systems/Chat/Widgets/ChatBox.xaml.cs
+++ Content.Client/UserInterface/Systems/Chat/Widgets/ChatBox.xaml.cs
@@ -3,6 +3,7 @@ using Content.Goobstation.Common.CCVar;
 using Robust.Shared.Configuration;
 using System.Linq;
 // </Trauma>
+using Content.Client.UserInterface.Controls;
 using Content.Client.UserInterface.RichText;
 using Content.Client.UserInterface.Systems.Chat.Controls;
 using Content.Shared.Chat;
@@ -136,7 +137,21 @@ public partial class ChatBox : UIWidget, IEntityLinkClickHandler
     }
     */
 
-    /*
+    /// <summary>
+    /// Calls update function on every child <see cref="TextLinkLabel"/> in the ChatBox's OutputPanel "Contents"
+    /// </summary>
+    public void UpdateTextLinkLabelProperties(SharedChatSystem _sharedChatSys)
+    {
+        foreach (var contentsChild in Contents.Children)
+        {
+            if (contentsChild is TextLinkLabel textLinkLabel)
+            {
+                textLinkLabel.UpdateLabelProperties(_sharedChatSys);
+            }
+        }
+    }
+
+    /* Trauma
     private void OnChannelFilter(ChatChannel channel, bool active)
     {
         Contents.Clear();
